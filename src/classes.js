f40a2c81578cb582295e5e81795e0f9162a1e76b
tmdwnsyang <95985696+tmdwnsyang@users.noreply.github.com>
2022-11-15 19:39:22 -0800

    Elapsed Time implementation
    The elapsed time for all users is updated and displayed in real-time.
    
    NOTE *deprecate hours for elapsedTime*

--- src/classes.js
+++ src/classes.js
@@ -13,9 +13,9 @@ function user(
   email,
   phoneNumber,
   srcAddressL1,
-  srcAddressL2 = ' ',
+  srcAddressL2 = " ",
   dstAddressL1,
-  dstAddressL2 = ' '
+  dstAddressL2 = " "
 ) {
   this.token = token;
   this.name = name;
@@ -27,60 +27,67 @@ function user(
     dstAddressL1: dstAddressL1,
     dstAddressL2: dstAddressL2,
   };
-  this.checkInTime = function () {
-    const d = new Date();
-    const obj = {
-      dateObj: d /* In case we need to perform date 
-                                     arithmetics, such as getElapsedTime() */,
-      hour: d.getHours(),
-      minute: d.getMinutes(),
-    };
 
-    this.checkInTime = obj;
-  };
-
-  this.setCheckOutTime = function () {
-    const d = new Date();
-    const obj = {
-      dateObj: d,
-      hour: d.getHours(),
-      minute: d.getMinutes(),
-    };
-    this.checkOutTime = obj;
-  };
-  /* Calculate the difference in time, returns the difference in minutes */
-  this.getElapsedTime = function () {
-    if (this.checkInTime === undefined || this.checkOutTime === undefined) {
-      console.log('Please set the checkin/checkout time first');
-      return;
-    }
-    const difference = Math.abs(
-      this.checkInTime.dateObj - this.checkOutTime.dateObj
-    );
-
-    return difference / 60000;
-  };
   /* fields */
   this.assigned = true;
-  this.pairedWalkers= {
-    walker1 :'',
-    walker2: '',
+  this.pairedWalkers = {
+    walker1Token: "",
+    walker2Token: "",
   };
   this.checkInTime = {
-      dateObj: new Date(),
-      hour: 0,
-      minute: 0,
-    };
+    dateObj: new Date(),
+    hour: 0,
+    minute: 0,
+  };
   this.checkOutTime = {
     dateObj: new Date(),
     hour: 0,
     minute: 0,
   };
-  this.timeElapsed = {
+  this.elapsedTime = {
     dateObj: new Date(),
     hour: 0,
     minute: 0,
-  }
+  };
 }
 
-export { user };
+/* Calculate the difference in time, returns the difference in minutes */
+/**
+ * @function userGetElapsedTime
+ * @param {user} userObj
+ * @returns elapsedTime object
+ * @brief
+ */
+const userGetElapsedTime = function (userObj) {
+  // console.log(userObj);
+  // console.log(userObj.checkInTime.dateObj.getMilliseconds());
+  let difference = new Date(Math.abs(new Date() - userObj.checkInTime.dateObj));
+  userObj.elapsedTime.hour = difference.getHours();
+  userObj.elapsedTime.minute = difference.getMinutes();
+  return userObj;
+};
+
+const userSetCheckOutTime = function (userObj) {
+  const d = new Date();
+  const obj = {
+    dateObj: d,
+    hour: d.getHours(),
+    minute: d.getMinutes(),
+  };
+  userObj.checkOutTime = obj;
+  return userObj;
+};
+
+const userSetCheckInTime = function (userObj) {
+  const d = new Date();
+  const obj = {
+    dateObj: d /* In case we need to perform date 
+                                   arithmetics, such as getElapsedTime() */,
+    hour: d.getHours(),
+    minute: d.getMinutes(),
+  };
+  userObj.checkInTime = obj;
+  return userObj;
+};
+
+export { user, userGetElapsedTime, userSetCheckInTime, userSetCheckOutTime };
--- src/demo.js
+++
@@ -1,8 +0,0 @@
-let string = 'myName';
-
-
-// creates a reference to a td element with class 'time'
-let time = document.querySelector('td');
-
-// change the textContent to write '11:59 PM'
-time.textContent ='11:00 PM';
