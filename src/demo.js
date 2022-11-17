0cf670e1b7902e9942cbcee3f63b27ccc79ef310
tmdwnsyang <95985696+tmdwnsyang@users.noreply.github.com>
2022-11-16 19:58:39 -0800

    Revert "Sam"

---
+++ src/demo.js
@@ -0,0 +1,8 @@
+let string = 'myName';
+
+
+// creates a reference to a td element with class 'time'
+let time = document.querySelector('td');
+
+// change the textContent to write '11:59 PM'
+time.textContent ='11:00 PM';
