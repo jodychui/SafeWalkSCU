4333dc911535f1c59fc1e95d5e2dff3207c2302e
Kristina Yin <kristinacyin@gmail.com>
2022-11-23 12:39:30 -0800

    only one checkbox can be checked in assign group popup

--- src/index.js
+++ src/index.js
@@ -832,3 +832,15 @@ $('#walkersPopUp').on('shown.bs.modal', function () {
 $('#deletePopUp').on('shown.bs.modal', function () {
   $('#myInput').trigger('focus')
 })
+
+$(document).ready(function () {
+  $('input[type="checkbox"]').on('change', function () {
+    var checkedValue = $(this).prop('checked');
+    // uncheck sibling checkboxes (checkboxes on the same row)
+    $(this).closest('tr').find('input[type="checkbox"]').each(function () {
+      $(this).prop('checked', false);
+    });
+    $(this).prop("checked", checkedValue);
+
+  });
+});
